0011486d83cb23a162527b60f9410583bfeeb204
Paul Kilgo <paulkilgo@gmail.com>
2013-12-27 01:59:37 -0500

    properly use argparse subcommands

--- pyprika/commands.py
+++ pyprika/commands.py
@@ -1,12 +1,13 @@
 from . import load
 from .pprint import pprint_recipe
 from .exceptions import ParseError
-from abc import ABCMeta, abstractmethod
+from abc import ABCMeta, abstractmethod, abstractproperty
 from argparse import ArgumentParser
 import glob
 import os
 import sys
 import yaml
+import re
 
 def _suggest(matches, cutoff=5, conjunct=' or ', sep=', ', ellipsis='...'):
   assert len(matches) > 1
@@ -23,8 +24,11 @@ class _Registry(object):
     self.recipes = {}
     self.search(os.path.curdir)
     config_path = os.path.expanduser("~/.pyprika")
-    with open(config_path) as fp:
-      self.config = yaml.load(fp)
+    if os.path.exists(config_path):
+      with open(config_path) as fp:
+        self.config = yaml.load(fp)
+    else:
+      self.config = {}
     if not isinstance(self.config, dict):
       sys.stderr.write("warning: invalid user configuration; skipping")
       self.config = {}
@@ -66,15 +70,47 @@ class CommandError(Exception):
 class Command(object):
   __metaclass__ = ABCMeta
 
-  @abstractmethod
-  def execute(self, argv):
+  @abstractproperty
+  def help(self):
+    raise NotImplementedError
+  
+  @abstractproperty
+  def name(self):
     raise NotImplementedError
 
-class Show(Command):
+  @abstractmethod
+  def execute(self, ns):
+    raise NotImplementedError
 
-  def execute(self, argv):
-    parser = self._get_parser()
-    ns = parser.parse_args(argv)
+  @abstractmethod
+  def setup_parser(self, subparsers):
+    raise NotImplementedError
+
+class Search(Command):
+  name = 'search'
+  help = 'look up recipes by name'
+
+  def execute(self, ns):
+    flags = 0
+    if ns.ignore_case:
+      flags |= re.I
+
+    recipes = [r for r in registry.recipes.itervalues()
+               if re.search(ns.term, r.name, flags)]
+    for r in recipes:
+      print r.name
+
+  def setup_parser(self, parser):
+    parser.add_argument('term', type=str,
+                        help='what to search for in recipe')
+    parser.add_argument('-i', '--ignore-case', action='store_true',
+                        help='ignore text case while searching')
+
+class Show(Command):
+  name = 'show'
+  help = 'show recipe contents'
+
+  def execute(self, ns):
     keys = [k for k in registry.recipes.keys() if k.startswith(ns.name)]
     if len(keys) == 1:
       pprint_recipe(registry.recipes[keys[0]])
@@ -83,31 +119,31 @@ class Show(Command):
     else:
       raise CommandError("no match found for '%s'" % ns.name)
 
-  def _get_parser(self):
-    parser = ArgumentParser()
-    parser.add_argument('name', type=str)
-    return parser
+  def setup_parser(self, parser):
+    parser.add_argument('name', type=str,
+                        help='name (or prefix of name) of recipe to show')
 
-COMMANDS = {
-  'show': Show(),
-}
+COMMANDS = (
+  Show(),
+  Search(),
+)
 
 def main():
   try:
     parser = get_parser()
+    subparsers = parser.add_subparsers()
+    for cmd in COMMANDS:
+      p = subparsers.add_parser(cmd.name, help=cmd.help)
+      cmd.setup_parser(p)
+      p.set_defaults(func=cmd.execute)
     ns = parser.parse_args()
-    cmd = COMMANDS.get(ns.subcommand)
-    if cmd is None:
-      raise CommandError("No such command: %s" % ns.subcommand)
-    cmd.execute(ns.argv)
+    ns.func(ns)
   except CommandError as e:
     sys.stderr.write(e.message + "\n")
     sys.exit(e.exitcode)
 
 def get_parser():
   parser = ArgumentParser(description='Recipe management utility')
-  parser.add_argument('subcommand', type=str)
-  parser.add_argument('argv', type=str, nargs='*')
   return parser
 
 if __name__ == "__main__":
