c398eab1d419b4a447662f0dd02a9f2aaa66e244
Paul Kilgo <paulkilgo@gmail.com>
2013-12-31 01:55:49 -0500

    change spec of pyprika.load
    no other module supports loading file paths so it seems best not to support
    it

--- pyprika/__init__.py
+++ pyprika/__init__.py
@@ -13,7 +13,23 @@ from .ingredient import Ingredient
 from .quantity import Quantity
 from .recipe import Recipe
 
-def _loadfp(fp):
+def load(fp):
+  """ Load ``fp``, a file-like object 
+
+  The file is assumed to be a pyprika-compliant YAML document. If the document
+  contains a sequence, a list of ``Recipe`` objects will be returned.
+  Otherwise, a single ``Recipe`` object should be returned.
+
+  Note that this function wraps the underlying exceptions thrown by
+  :meth:`Recipe.from_dict` under the assumption it is due to a malformed
+  document, but the original traceback is preserved.
+
+  :param file-like fp: the file-like object containing the document to load
+  :raises LoadError: if there was an error in the loading of the document,
+                     usually indicative of a syntax error
+  :returns: the recipe data contained in the stream 
+  :rtype: :class:`Recipe` or list of :class:`Recipe`
+  """
   try:
     d = yaml.load(fp)
     if isinstance(d, (tuple, list)):
@@ -25,34 +41,6 @@ def _loadfp(fp):
   except:
     exc_type, exc, traceback = sys.exc_info()
     raise LoadError, LoadError(*exc.args, cause=exc), traceback
-
-def load(resource):
-  """ Load ``resource``, some mixed type.
-
-  If ``resource`` is a string it will be treated as a file path. Otherwise, it
-  will be treated as a file-like object.
-
-  The file is assumed to be a pyprika-compliant YAML document. If the document
-  contains a sequence, a list of ``Recipe`` objects will be returned.
-  Otherwise, a single ``Recipe`` object should be returned.
-
-  Note that this function wraps the underlying exceptions thrown by
-  :meth:`Recipe.from_dict` under the assumption it is due to a malformed
-  document, but the original traceback is preserved.
-
-  :param resource: the resource containing the document to load
-  :type str or file-like object resource:
-  :raises IOError: if there was an error opening the resource
-  :raises LoadError: if there was an error in the loading of the document,
-                     usually indicative of a syntax error
-  :returns: the recipe data contained in that resource
-  :rtype: :class:`Recipe` or list of :class:`Recipe`
-  """
-  if isinstance(resource, basestring):
-    with open(resource, 'r') as fp:
-      return _loadfp(fp)
-  else:
-    return _loadfp(resource)
   
 def loads(data):
   """ Load recipe from string data.
